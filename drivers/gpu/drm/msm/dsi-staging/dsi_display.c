90e257a42f65bb6800a522b1ba9876f84aaaabde
Demon000 <demonsingur@gmail.com>
2020-06-08 17:16:57 +0200

    drm: msm: dsi-staging: Cleanup notifier implementation
    Change-Id: If60a789b666cd2c5ab767406d2aeabb03f38ba5b

--- drivers/gpu/drm/msm/dsi-staging/dsi_display.c
+++ drivers/gpu/drm/msm/dsi-staging/dsi_display.c
@@ -1335,7 +1335,7 @@ int dsi_display_set_power(struct drm_connector *connector,
 {
 	struct dsi_display *display = disp;
 	int rc = 0;
-	struct msm_drm_notifier g_notify_data;
+	struct msm_drm_notifier notify_data;
 	struct drm_device *dev = NULL;
 	int event = 0;
 
@@ -1350,7 +1350,8 @@ int dsi_display_set_power(struct drm_connector *connector,
 	}
 	dev = connector->dev;
 	event = dev->doze_state;
-	g_notify_data.data = &event;
+	notify_data.data = &power_mode;
+	notify_data.id = MSM_DRM_PRIMARY_DISPLAY;
 
 	switch (power_mode) {
 	case SDE_MODE_DPMS_LP1:
