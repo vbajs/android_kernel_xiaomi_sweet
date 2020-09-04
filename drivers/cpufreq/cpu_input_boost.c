9ff215f2e594c9e6077d7fcac04bd6d1bfa92e5f
Adithya R <gh0strider.2k18.reborn@gmail.com>
2020-09-04 15:08:18 +0530

    cpu_input_boost: Get max boost freq from policy->cpuinfo.max_freq
     * Max boost freq should always be the max possible freq of the
       CPU, there's no need to have a separate config for it.
    
    Change-Id: Ia0036c6fddd961d6fe0ccb96f186b1a628e2d66e
    Signed-off-by: Richard Raya <rdxzv.dev@gmail.com>

--- drivers/cpufreq/cpu_input_boost.c
+++ drivers/cpufreq/cpu_input_boost.c
@@ -87,18 +87,6 @@ static unsigned int get_min_freq(struct cpufreq_policy *policy)
 	return max(freq, policy->min);
 }
 
-static unsigned int get_max_boost_freq(struct cpufreq_policy *policy)
-{
-	unsigned int freq;
-
-	if (cpumask_test_cpu(policy->cpu, cpu_lp_mask))
-		freq = CONFIG_MAX_BOOST_FREQ_LP;
-	else
-		freq = CONFIG_MAX_BOOST_FREQ_PERF;
-
-	return min(freq, policy->max);
-}
-
 static void update_online_cpu_policy(void)
 {
 	unsigned int cpu;
@@ -246,7 +234,7 @@ static int cpu_notifier_cb(struct notifier_block *nb, unsigned long action,
 
 	/* Boost CPU to max frequency for max boost */
 	if (test_bit(MAX_BOOST, &b->state)) {
-		policy->min = get_max_boost_freq(policy);
+		policy->min = policy->cpuinfo.max_freq;
 		return NOTIFY_OK;
 	}
 
