62df0221f18cd396a978e8aec973ce11903b60b9
Sultan Alsawaf <sultan@kerneltoast.com>
2023-07-04 20:46:18 -0700

    sched/fair: Always update CPU capacity when load balancing
    Limiting CPU capacity updates, which are quite cheap, results in worse
    balancing decisions during opportunistic balancing (e.g., SD_BALANCE_WAKE).
    This causes opportunistic placement decisions to be skewed using stale CPU
    capacity data, and when a CPU isn't idling much, its capacity suffers from
    even more staleness since the only exception to the 100 ms capacity update
    ratelimit is a CPU exiting idle.
    
    Since the capacity updates are cheap, always do it when load balancing in
    order to improve opportunistic task placement decisions.
    
    Signed-off-by: Sultan Alsawaf <sultan@kerneltoast.com>
    [shygosh: Adapted for msm-4.14]
    Signed-off-by: shygosh <shygosh@proton.me>

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -10435,6 +10435,7 @@ static inline void update_sd_lb_stats(struct lb_env *env, struct sd_lb_stats *sd
 		if (local_group) {
 			sds->local = sg;
 			sgs = local;
+			update_group_capacity(env->sd, env->dst_cpu);
 		}
 
 		update_sg_lb_stats(env, sg, load_idx, local_group, sgs,
@@ -11599,9 +11600,7 @@ static int idle_balance(struct rq *this_rq, struct rq_flags *rf)
 		u64 t0, domain_cost;
 
 		if (!(sd->flags & SD_LOAD_BALANCE)) {
-			if (time_after_eq(jiffies,
-					  sd->groups->sgc->next_update))
-				update_group_capacity(sd, this_cpu);
+			update_group_capacity(sd, this_cpu);
 			continue;
 		}
 
@@ -12015,9 +12014,7 @@ static void rebalance_domains(struct rq *rq, enum cpu_idle_type idle)
 			continue;
 
 		if (!(sd->flags & SD_LOAD_BALANCE)) {
-			if (time_after_eq(jiffies,
-					  sd->groups->sgc->next_update))
-				update_group_capacity(sd, cpu);
+			update_group_capacity(sd, cpu);
 			continue;
 		}
 
