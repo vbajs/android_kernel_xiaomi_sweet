e0c0ac383ec2e91078779fface20095fb302c126
Yuan Si <do4suki@gmail.com>
2024-04-04 00:53:44 +0800

    mm/vmscan: Bump swappiness
    Change-Id: I19e4a6e2566e2d6f96ce84045f240f789c2d2f15
    Signed-off-by: Yuan Si <do4suki@gmail.com>
    Signed-off-by: NotZeetaa <rodrigo2005contente@gmail.com>

--- mm/vmscan.c
+++ mm/vmscan.c
@@ -183,7 +183,7 @@ unsigned long sysctl_clean_min_kbytes __read_mostly = CONFIG_CLEAN_MIN_KBYTES;
 /*
  * From 0 .. 200.  Higher means more swappy.
  */
-int vm_swappiness = 100;
+int vm_swappiness = 160;
 #ifdef CONFIG_OPLUS_MM_HACKS
 /*
  * Direct reclaim swappiness, values range from 0 .. 60. Higher means more swappy.
