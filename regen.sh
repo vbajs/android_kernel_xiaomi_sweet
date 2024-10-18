1438afda4e30db88ff3caab5571a68026da907da
Yahya Wessam <yahyawessam2002@gmail.com>
2024-10-18 16:14:07 +0300

    Scripts: hardcode regen.sh to sweet only

--- regen.sh
+++ regen.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 
 # Array of device names
-devices=("sweet" "courbet" "tucana" "toco" "phoenix" "davinci")
-#devices=("sweet" "courbet")
+#devices=("sweet" "courbet" "tucana" "toco" "phoenix" "davinci")
+devices=("sweet")
 
 # Loop through the devices
 for device in "${devices[@]}"
 do
     echo "Generating defconfig for $device"
-    make ARCH=arm64 O=out "${device}"_defconfig
+    make ARCH=arm64 O=out "vendor/${device}"_defconfig
     make ARCH=arm64 O=out menuconfig
-    cp out/.config "arch/arm64/configs/${device}_defconfig"
+    cp out/.config "arch/arm64/configs/vendor/${device}_defconfig"
     rm -rf out
     echo "Defconfig generated for $device"
     echo ""
 done
 
-echo "Defconfig generation completed for all devices."
+echo "Defconfig generation completed."
