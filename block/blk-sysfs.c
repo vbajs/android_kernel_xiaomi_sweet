8d964a54ef5b43e8c33ed7f5b407a99606c124c9
Richard Raya <rdxzv.dev@gmail.com>
2024-12-08 10:38:22 -0300

    mm: Protect readahead size from init
    Change-Id: I1f105969bcdd756cd68e8692fb3419aa72b07b5e
    Signed-off-by: Richard Raya <rdxzv.dev@gmail.com>

--- block/blk-sysfs.c
+++ block/blk-sysfs.c
@@ -107,7 +107,7 @@ queue_ra_store(struct request_queue *q, const char *page, size_t count)
 	if (ret < 0)
 		return ret;
 
-	if (task_is_booster(current))
+	if (task_is_booster(current) || !strcmp(current->comm, "init"))
 		ra_kb = VM_MAX_READAHEAD;
 
 	q->backing_dev_info->ra_pages = ra_kb >> (PAGE_SHIFT - 10);
