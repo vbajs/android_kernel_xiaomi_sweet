6382cc635c42f8124a412ed000743dcbfab59ca8
shygosh <shygosh@proton.me>
2025-01-30 01:24:11 +0700

    mm/page_alloc: watermark_scale_factor=200
    Signed-off-by: shygosh <shygosh@proton.me>

--- mm/page_alloc.c
+++ mm/page_alloc.c
@@ -321,7 +321,7 @@ compound_page_dtor * const compound_page_dtors[] = {
  */
 int min_free_kbytes = 1024;
 int user_min_free_kbytes = -1;
-int watermark_scale_factor = 20;
+int watermark_scale_factor = 200;
 
 /*
  * Extra memory for the system to try freeing. Used to temporarily
