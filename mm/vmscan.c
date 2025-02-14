5e3d7de156b48b91bebe12e4988a339c30a0ce2a
Yahya Wessam <yahyawessam2002@gmail.com>
2025-02-14 14:46:34 +0200

    mm: Revert all swappiness commits
    We'll switch to oplus mm hacks
    This reverts the following commits:
    e4bc9ec188762ca677d3bdbb763a3f6ebad1fb59 mm: vmscan: bump swappiness to 100
    c0cb321673ae0f405612e45845bb6e6c3530a9f8 mm: memcontrol: set the correct memcg swappiness restriction
    5631297a69e409c44f3e22b593e40f01ca0225f7 mm: allow swappiness that prefers reclaiming anon over the file workingset

--- mm/vmscan.c
+++ mm/vmscan.c
@@ -181,9 +181,9 @@ unsigned long sysctl_clean_low_kbytes __read_mostly = CONFIG_CLEAN_LOW_KBYTES;
 unsigned long sysctl_clean_min_kbytes __read_mostly = CONFIG_CLEAN_MIN_KBYTES;
 
 /*
- * From 0 .. 200.  Higher means more swappy.
+ * From 0 .. 100.  Higher means more swappy.
  */
-int vm_swappiness = 100;
+int vm_swappiness = 70;
 /*
  * The total number of pages which are beyond the high watermark within all
  * zones.
