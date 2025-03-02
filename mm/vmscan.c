d0a428a01aeec8bf620e39005ead4a56bc1df0fc
Yahya Wessam <yahyawessam2002@gmail.com>
2025-03-02 23:51:40 +0200

    Revert "mm: Revert all swappiness commits"
    This reverts commit 5e3d7de156b48b91bebe12e4988a339c30a0ce2a.
    Let's use em alongside oplus mm hacks.

--- mm/vmscan.c
+++ mm/vmscan.c
@@ -181,9 +181,9 @@ unsigned long sysctl_clean_low_kbytes __read_mostly = CONFIG_CLEAN_LOW_KBYTES;
 unsigned long sysctl_clean_min_kbytes __read_mostly = CONFIG_CLEAN_MIN_KBYTES;
 
 /*
- * From 0 .. 100.  Higher means more swappy.
+ * From 0 .. 200.  Higher means more swappy.
  */
-int vm_swappiness = 70;
+int vm_swappiness = 100;
 #ifdef CONFIG_OPLUS_MM_HACKS
 /*
  * Direct reclaim swappiness, values range from 0 .. 60. Higher means more swappy.
