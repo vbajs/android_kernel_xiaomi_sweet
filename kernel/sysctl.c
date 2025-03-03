aabd7e6e41367dc977974c843401b30719706103
Yahya Wessam <yahyawessam2002@gmail.com>
2025-03-03 11:02:25 +0200

    Revert "BACKPORT: [v7] mm: Proactive compaction"
    This reverts commit d59e1eb4116ac87c01aeeaa8e144edddb2a9291f.

--- kernel/sysctl.c
+++ kernel/sysctl.c
@@ -1728,26 +1728,17 @@ static struct ctl_table vm_table[] = {
 #ifdef CONFIG_COMPACTION
 	{
 		.procname	= "compact_memory",
-		.data		= NULL,
+		.data		= &sysctl_compact_memory,
 		.maxlen		= sizeof(int),
 		.mode		= 0200,
 		.proc_handler	= sysctl_compaction_handler,
-	},
-	{
-		.procname	= "compaction_proactiveness",
-		.data		= &sysctl_compaction_proactiveness,
-		.maxlen		= sizeof(sysctl_compaction_proactiveness),
-		.mode		= 0644,
-		.proc_handler	= compaction_proactiveness_sysctl_handler,
-		.extra1		= &zero,
-		.extra2		= &one_hundred,
 	},
 	{
 		.procname	= "extfrag_threshold",
 		.data		= &sysctl_extfrag_threshold,
 		.maxlen		= sizeof(int),
 		.mode		= 0644,
-		.proc_handler	= proc_dointvec_minmax,
+		.proc_handler	= sysctl_extfrag_handler,
 		.extra1		= &min_extfrag_threshold,
 		.extra2		= &max_extfrag_threshold,
 	},
