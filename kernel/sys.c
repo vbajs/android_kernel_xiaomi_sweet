25874a0458de9933bfeaa6a762d1aeaade09eba2
backslashxx <118538522+backslashxx@users.noreply.github.com>
2025-03-11 11:07:47 +0800

    sys_newuname: only spoof for root
    [vbajs]
    * Adapt to old 6.6 uname spoof

--- kernel/sys.c
+++ kernel/sys.c
@@ -1189,11 +1189,12 @@ SYSCALL_DEFINE1(newuname, struct new_utsname __user *, name)
 
 	down_read(&uts_sem);
 	memcpy(&tmp, utsname(), sizeof(tmp));
-	if (!strncmp(current->comm, "netbpfload", 10) &&
-			current->pid != netbpfload_pid) {
+	if (current_uid().val == 0 && 
+		(!strncmp(current->comm, "netbpfload", 10) &&
+		current->pid != netbpfload_pid)) {
 		netbpfload_pid = current->pid;
 		strcpy(tmp.release, "6.6.40");
-		pr_debug("fake uname: %s/%d release=%s\n",
+		pr_info("fake uname: %s/%d release=%s\n",
 				current->comm, current->pid, tmp.release);
 	}
 	up_read(&uts_sem);
