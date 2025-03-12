36a4d4b0b1ce0ecec0d9949431a90fa7b6f5a380
Yahya Wessam <yahyawessam2002@gmail.com>
2025-03-12 23:32:00 +0200

    Revert "power: supply: ti: ln8000 pd, cp changes"
    This reverts commit 5b1d8c30b03482e4a9dde44a793b27ffeddad038.

--- drivers/power/supply/ti/cp_qc30.c
+++ drivers/power/supply/ti/cp_qc30.c
@@ -1172,7 +1172,6 @@ static void cp_workfunc(struct work_struct *work)
 	if (pm_state.usb_present == 0) {
 		cp_set_qc_bus_protections(HVDCP3_NONE);
 		cp_set_fake_hvdcp3(false);
-		pm_state.state = CP_STATE_DISCONNECT;
 		return;
 	}
 
