af89c61a737f3287629c2305ef2ed69ddcc82251
Yahya Wessam <yahyawessam2002@gmail.com>
2025-03-13 01:04:10 +0200

    Revert "schedutil: Update schedutial up/down limit rate."
    This reverts commit ff7f70cee0076d6e85538e153e841f4ef1267afd.

--- kernel/sched/cpufreq_schedutil.c
+++ kernel/sched/cpufreq_schedutil.c
@@ -22,7 +22,7 @@
 
 #define SUGOV_KTHREAD_PRIORITY	50
 #define UP_RATE_LIMIT_US		(500)
-#define DOWN_RATE_LIMIT_US		(4000)
+#define DOWN_RATE_LIMIT_US		(20000)
 
 struct sugov_tunables {
 	struct gov_attr_set attr_set;
